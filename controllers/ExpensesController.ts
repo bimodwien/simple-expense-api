9795a1a589ca5a0cfa59f9802036fadce5db49ab
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2024-05-07 15:28:03 +0700

    CRUD done

--- controllers/ExpensesController.ts
+++ controllers/ExpensesController.ts
@@ -13,9 +13,7 @@ class ExpensesController {
         data: allExpenses,
       });
     } catch (error) {
-      res.status(500).send({
-        message: "Internal Server Error",
-      });
+      res.send({ message: "Internal Server Error" }).status(500);
     }
   }
 
@@ -25,7 +23,6 @@ class ExpensesController {
       const expense = expenses.find((exp) => {
         return exp.id === Number(id);
       });
-
       if (!expense) {
         return res.status(404).send({
           message: "Data Not Found",
@@ -41,11 +38,68 @@ class ExpensesController {
     }
   }
 
-  static createExpense(req: Request, res: Response) {}
+  static createExpense(req: Request, res: Response) {
+    try {
+      const data = {
+        name: req.body.name as string,
+        nominal: Number(req.body.nominal) as number,
+        category: req.body.category as string,
+      };
+      const id = expenses.length ? expenses[expenses.length - 1].id + 1 : 1;
+      const newData = { id, ...data };
+      expenses.push(newData);
+      res.status(201).send({
+        message: "Data insert success",
+        data: newData,
+      });
+    } catch (error) {
+      res.send({ message: "Internal Server Error" }).status(500);
+    }
+  }
 
-  static editExpense(req: Request, res: Response) {}
+  static editExpense(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      const data = {
+        name: req.body.name as string,
+        nominal: Number(req.body.nominal) as number,
+        category: req.body.category as string,
+      };
+      const idx = expenses.findIndex((exp) => {
+        return exp.id === Number(id);
+      });
+      if (idx === -1) {
+        return res.status(404).send({ message: "Data not found" });
+      } else {
+        expenses[idx] = { ...expenses[idx], ...data };
+        res.status(201).send({
+          message: "Successfully change data",
+          data: expenses[idx],
+        });
+      }
+    } catch (error) {
+      res.send({ message: "Internal Server Error" }).status(500);
+    }
+  }
 
-  static deleteExpense(req: Request, res: Response) {}
+  static deleteExpense(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      const idx = expenses.findIndex((exp) => {
+        return exp.id === Number(id);
+      });
+      if (idx === -1) {
+        res.status(404).send({ message: "Data not found" });
+      } else {
+        expenses.splice(idx, 1);
+        res.status(201).send({
+          message: "Succesfully delete data",
+        });
+      }
+    } catch (error) {
+      res.send({ message: "Internal Server Error" }).status(500);
+    }
+  }
 
   static totalByDateRange(req: Request, res: Response) {}
 
