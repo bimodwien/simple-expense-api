75f2e76e9de332e2caf5f3a855e67f4b3f178844
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2024-05-07 14:43:06 +0700

    show data done

--- controllers/ExpensesController.ts
+++ controllers/ExpensesController.ts
@@ -1,19 +1,55 @@
 "use strict";
+import { Request, Response } from "express";
+import expenses from "../model/expenses";
 
 class ExpensesController {
-  static showList() {}
+  static showList(req: Request, res: Response) {
+    try {
+      const allExpenses = expenses.map((expence) => {
+        return expence;
+      });
+      res.status(200).send({
+        message: "Fetch all expenses",
+        data: allExpenses,
+      });
+    } catch (error) {
+      res.status(500).send({
+        message: "Internal Server Error",
+      });
+    }
+  }
 
-  static showDetail() {}
+  static showDetail(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      const expense = expenses.find((exp) => {
+        return exp.id === Number(id);
+      });
 
-  static createExpense() {}
+      if (!expense) {
+        return res.status(404).send({
+          message: "Data Not Found",
+        });
+      } else {
+        res.status(200).send({
+          message: "Fetch data with id",
+          data: expense,
+        });
+      }
+    } catch (error) {
+      res.send({ message: "Internal Server Error" }).status(500);
+    }
+  }
 
-  static editExpense() {}
+  static createExpense(req: Request, res: Response) {}
 
-  static deleteExpense() {}
+  static editExpense(req: Request, res: Response) {}
 
-  static totalByDateRange() {}
+  static deleteExpense(req: Request, res: Response) {}
 
-  static totalByCategory() {}
+  static totalByDateRange(req: Request, res: Response) {}
+
+  static totalByCategory(req: Request, res: Response) {}
 }
 
 export default ExpensesController;
